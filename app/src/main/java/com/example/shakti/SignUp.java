4b5d4e8a9456834e9186ad0eb5391380c32c2cd9
Yashshri-22 <yashshri2205@gmail.com>
2025-03-06 00:04:07 +0530

    Signup Authentication Successful

--- app/src/main/java/com/example/shakti/SignUp.java
+++ app/src/main/java/com/example/shakti/SignUp.java
@@ -1,32 +1,45 @@
 package com.example.shakti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUp extends AppCompatActivity {
-    private EditText usernameEditText, passwordEditText, confirmPasswordEditText;
+    private EditText emailEditText, passwordEditText, confirmPasswordEditText;
     private Button signupButton;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-        usernameEditText = findViewById(R.id.username_edittext);
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.email_edittext);
         passwordEditText = findViewById(R.id.password_edittext);
         confirmPasswordEditText = findViewById(R.id.confirm_password_edittext);
         signupButton = findViewById(R.id.btn_signup);
+        progressBar = findViewById(R.id.progressbar);
 
         Button signup = findViewById(R.id.btn_signup);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 if (validateInputs()) {
                     // If all conditions are met, navigate to HomeActivity
                     Intent intent = new Intent(SignUp.this, Dashboard.class);
@@ -38,11 +51,11 @@ public class SignUp extends AppCompatActivity {
     }
 
     private boolean validateInputs() {
-        String username = usernameEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String confirmPassword = confirmPasswordEditText.getText().toString().trim();
 
-        if (username.isEmpty()) {
+        if (email.isEmpty()) {
             showToast("Username cannot be empty");
             return false;
         }
@@ -58,6 +71,23 @@ public class SignUp extends AppCompatActivity {
             showToast("Passwords do not match");
             return false;
         }
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignUp.this, "Account Created.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         return true;
     }
 
