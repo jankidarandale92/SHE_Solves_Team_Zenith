420a92bf3a0238f74e86511969f33446e1b0fdbf
Yashshri-22 <yashshri2205@gmail.com>
2025-03-06 00:31:40 +0530

    Login Authentication Complete

--- app/src/main/java/com/example/shakti/LoginPage.java
+++ app/src/main/java/com/example/shakti/LoginPage.java
@@ -1,14 +1,109 @@
 package com.example.shakti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginPage.this, Dashboard.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.email_edittext);
+        passwordEditText = findViewById(R.id.password_edittext);
+        loginButton = findViewById(R.id.btn_login);
+        progressBar = findViewById(R.id.progressbar);
+        textView = findViewById(R.id.signup_text);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                validateInputs();
+            }
+        });
+    }
+
+    private boolean validateInputs() {
+        progressBar.setVisibility(View.VISIBLE);
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            showToast("Username cannot be empty");
+            return false;
+        }
+        if (password.isEmpty()) {
+            showToast("Password cannot be empty");
+            return false;
+        }
+        if (password.length() < 6) {
+            showToast("Password must be at least 6 characters long");
+            return false;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            showToast("Login Successful.");
+                            Intent intent = new Intent(LoginPage.this, Dashboard.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            showToast("Authentication failed.");
+                        }
+                    }
+                });
+
+        return true;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
+
