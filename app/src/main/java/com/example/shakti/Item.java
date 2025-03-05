f8e9443b97bd7bc3d105c1617df257128a000909
Yashshri-22 <yashshri2205@gmail.com>
2025-03-05 20:30:58 +0530

    Created the Structure of the card by using Item Class

---
+++ app/src/main/java/com/example/shakti/Item.java
@@ -0,0 +1,37 @@
+package com.example.shakti;
+
+public class Item {
+    String name;
+    int phone;
+    int image;
+
+    public Item(String name, int phone, int image) {
+        this.name = name;
+        this.phone = phone;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
