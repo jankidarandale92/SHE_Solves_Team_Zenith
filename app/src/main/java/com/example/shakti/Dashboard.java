44a65fb2ff399f89837167ce46f8457415b9252a
Yashshri-22 <yashshri2205@gmail.com>
2025-03-06 01:42:16 +0530

    Dashboard Linked with Helplines Page

--- app/src/main/java/com/example/shakti/Dashboard.java
+++ app/src/main/java/com/example/shakti/Dashboard.java
@@ -1,14 +1,27 @@
 package com.example.shakti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
 
+    CardView helplines;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        helplines = findViewById(R.id.helpline);
+
+        helplines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, Helplines.class);
+                startActivity(intent);
+            }
+        });
     }
 }
