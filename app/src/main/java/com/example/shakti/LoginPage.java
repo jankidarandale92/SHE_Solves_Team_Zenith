c85e98c8f596b119500f8ac35ac40c24d28a9985
Yashshri-22 <yashshri2205@gmail.com>
2025-03-06 10:28:04 +0530

    Login Signup design modifications

--- app/src/main/java/com/example/shakti/LoginPage.java
+++ app/src/main/java/com/example/shakti/LoginPage.java
@@ -25,17 +25,17 @@ public class LoginPage extends AppCompatActivity {
     FirebaseAuth mAuth;
     TextView textView;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(LoginPage.this, Dashboard.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(LoginPage.this, Dashboard.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/shakti/SignUp.java
+++ app/src/main/java/com/example/shakti/SignUp.java
@@ -24,17 +24,17 @@ public class SignUp extends AppCompatActivity {
     ProgressBar progressBar;
     FirebaseAuth mAuth;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(SignUp.this, Dashboard.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(SignUp.this, Dashboard.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
